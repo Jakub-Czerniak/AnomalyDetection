818dc06e049600d95d8113b73ad243b2243a97f3
Krzysztof Jodłowiec <k.jodlowiec00@wp.pl>
2022-04-07 12:00:37 +0200

    Program dzielacy dane z czujnikow na rozne pliki.

---
+++ separate_data.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//* Podział CSV na oddzielne pliki, nazwa pliku = $id_czujnika + ".txt"
+// Do dzialania potrzebny jest utworzony folder o nazwie data
+void separate_data(ifstream& data_file)
+{
+    int id, old_id; 
+    string file_name;
+    string text; 
+    char c; 
+
+    ofstream new_file;
+
+    data_file >> id;
+
+    while (!data_file.eof())
+    {
+        file_name = "./data/" + to_string(id) + ".txt"; // tutaj tworzona jest nazwa pliku z danymi
+        new_file.open(file_name);
+        new_file << "ID czujnika: " << id << endl;
+
+        do
+        {
+            old_id = id;
+            data_file >> c;
+            getline(data_file,text);
+            new_file << text << endl;
+
+            data_file >> id;
+        } while (old_id == id);
+
+        new_file.close();
+    }
+
+    new_file.close();
+}
+
+int main(void)
+{
+    ifstream data_file("./Budynek_1_woda_historia.csv"); // tutaj wybierany jest plik, z ktorego czytamy
+    separate_data(data_file);
+}
