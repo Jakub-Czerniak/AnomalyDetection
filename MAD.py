32969962cc90d535a214d2370568e784e90a49b0
Kuba <kubaczarny3@gmail.com>
2022-05-18 19:29:48 +0200

    Liczniki wody

--- MAD.py
+++ MAD.py
@@ -1,40 +1,50 @@
 import numpy
 
 class mad: 
-    def __init__(self, X, minMAD, threshold):
+    def __init__(self, X, minMAD):
         self.X=X
         self.minMAD=numpy.float64(minMAD)
         self.MAD=0
-        self.threshold=threshold
+        self.threshold=0
         self.isAnomaly=False
 
     def __calcualteMAD(self):
         self.MAD=numpy.median(numpy.fabs(X-numpy.median(X)))
 
-    def detectAnomalies(self):
+    def detectAnomalies(self, threshold):
         self.__calcualteMAD()
-        print('MAD ' + self.MAD.astype(str))
         if(self.MAD<self.minMAD):
             self.MAD=self.minMAD
         MADn=1.4826*self.MAD
-        print('MADn ' + MADn.astype(str))
-        print('Last: ' + str(self.X[-1]))
         Z=numpy.fabs(self.X[-1]-numpy.median(self.X))/MADn
-        print('Z ' + Z.astype(str))
-        if(Z>self.threshold):
+        if( Z> threshold):
             self.isAnomaly=True
 
-def runMAD_onData(data,windowSize):
-    for i in range(0, len(data)-windowSize+1, 1):
-        X = [data[i:i+windowSize] ]
-        print(X[0])
-        m=mad(X[0], 0.01,3)
-        m.detectAnomalies()
+    def calculateThreshold(self):
+        self.__calcualteMAD()
+        if(self.MAD<self.minMAD):
+            self.MAD=self.minMAD
+        MADn=1.4826*self.MAD
+        self.threshold=numpy.fabs(self.X[-1]-numpy.median(self.X))/MADn
+        
+
+def runMAD_onData(data,windowSize, learningWindowSize):
+    threshold=0.0
+    for i in range (0, learningWindowSize-windowSize+1):
+        X = [data[i:i+windowSize]]
+        m=mad(X, 0.01)
+        m.calculateThreshold()
+        threshold = max ( m.threshold, threshold )
+        print(threshold)
+    for i in range(learningWindowSize-windowSize+1, len(data)-windowSize+1, 1):
+        X = [data[i:i+windowSize]]
+        m=mad(X, 0.01)        
+        m.detectAnomalies(threshold)
         if(m.isAnomaly==True):
             print('anomaly at', i+windowSize)
 
-#X=[0.5,0.5,0.5,0.5,0.5,0.5,0.55,0.55,0.55,0.55,0.6,0.6,0.6,0.6,0.65,0.65,0.65,0.65,0.65,0.8,0.8,0.8,0.9]
+X=[0.5,0.5,0.5,0.5,0.5,0.5,0.55,0.55,0.55,0.55,0.6,0.6,0.6,0.6,0.65,0.65,0.65,0.65,0.65,0.8,0.8,0.8,0.9]
 #X=[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,10,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,5]
-X=[1.0,2.0,2.5,4.0,5.0,5.0,100]
+#X=[1.0,2.0,2.5,4.0,5.0,5.0,100]
 
-runMAD_onData(X, 5)
+runMAD_onData(X, 11, 10)
