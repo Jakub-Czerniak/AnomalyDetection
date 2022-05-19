a1835b7d6234275ee1f538370901bf9184d87366
Kuba <kubaczarny3@gmail.com>
2022-05-19 14:19:32 +0200

    MAD dodano komunikat o wielkosci i rodzaju, obliczanie granic do wyswetlenia na wykresie

--- MAD.py
+++ MAD.py
@@ -7,25 +7,31 @@ class mad:
         self.MAD=0
         self.threshold=0
         self.isAnomaly=False
+        self.degree=0
+        self.upperBound=0
+        self.lowerBound=0
 
     def __calcualteMAD(self):
-        self.MAD=numpy.median(numpy.fabs(X-numpy.median(X)))
+        self.MAD=numpy.median(numpy.abs(X-numpy.median(X)))
 
     def detectAnomalies(self, threshold):
         self.__calcualteMAD()
         if(self.MAD<self.minMAD):
             self.MAD=self.minMAD
         MADn=1.4826*self.MAD
-        Z=numpy.fabs(self.X[-1]-numpy.median(self.X))/MADn
+        Z=numpy.abs(self.X[-1]-numpy.median(self.X))/MADn
+        self.upperBound=numpy.abs(numpy.median(self.X) + MADn * threshold)
+        self.lowerBound=numpy.abs(numpy.median(self.X) - MADn * threshold)
         if( Z> threshold):
             self.isAnomaly=True
+            self.degree=Z // threshold
 
     def calculateThreshold(self):
         self.__calcualteMAD()
         if(self.MAD<self.minMAD):
             self.MAD=self.minMAD
         MADn=1.4826*self.MAD
-        self.threshold=numpy.fabs(self.X[-1]-numpy.median(self.X))/MADn
+        self.threshold=numpy.abs(self.X[-1]-numpy.median(self.X))/MADn
         
 
 def runMAD_onData(data,windowSize, learningWindowSize):
@@ -36,14 +42,17 @@ def runMAD_onData(data,windowSize, learningWindowSize):
         m.calculateThreshold()
         threshold = max ( m.threshold, threshold )
         print(threshold)
+    threshold=1.1*threshold
     for i in range(learningWindowSize-windowSize+1, len(data)-windowSize+1, 1):
         X = [data[i:i+windowSize]]
-        m=mad(X[0], 0.01)        
+        m=mad(X[0], 0.01)
         m.detectAnomalies(threshold)
         if(m.isAnomaly==True):
-            print('anomaly at', i+windowSize)
+            #anomaliesX.append(X_time[i + windowSize])
+            #anomaliesY.append(data[i + windowSize])
+            print('upper bound breach' if X[0][-1]>m.upperBound else 'lower bound breach','anomaly of rank ', m.degree if m.degree<5 else 5, ' at ', i,'upper bound:', m.upperBound,'lower bound:', m.lowerBound ,'value:', X[0][-1])
 
-X=[0.5,0.5,0.5,0.5,0.5,0.5,0.55,0.55,0.55,0.55,0.6,0.6,0.6,0.6,0.65,0.65,0.65,0.65,0.65,0.8,0.8,0.8,0.9]
+X=[0.5,0.5,0.5,0.5,0.5,0.5,0.55,0.55,0.55,0.55,0.6,0.6,0.6,0.6,0.1,0.65,0.65,0.65,0.65,0.65,0.8,0.8,0.8,0.9]
 #X=[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,10,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,5]
 #X=[1.0,2.0,2.5,4.0,5.0,5.0,100]
 
--- wyswietlanie.py
+++ wyswietlanie.py
@@ -19,48 +19,62 @@ def calculate_mean_delta(dat, date,  num):
 
 ###############################################
 #### Algorytm
-class mad:
-
-    def __init__(self, X, minMAD, threshold):
-        self.X = X
-        self.minMAD = numpy.float64(minMAD)
-        self.MAD = 0
-        self.threshold = threshold
-        self.isAnomaly = False
+class mad: 
+    def __init__(self, X, minMAD):
+        self.X=X
+        self.minMAD=numpy.float64(minMAD)
+        self.MAD=0
+        self.threshold=0
+        self.isAnomaly=False
+        self.degree=0
+        self.upperBound=0
+        self.lowerBound=0
 
     def __calcualteMAD(self):
-        self.MAD = numpy.median(numpy.abs(X-numpy.median(X)))
+        self.MAD=numpy.median(numpy.abs(X-numpy.median(X)))
 
-
-    def detectAnomalies(self):
+    def detectAnomalies(self, threshold):
         self.__calcualteMAD()
-        # print('MAD ' + self.MAD.astype(str))
-        if self.MAD < self.minMAD:
-            self.MAD = self.minMAD
-        MADn = 1.4826 * self.MAD
-        # print('MADn ' + MADn.astype(str))
-        # print('Last: ' + str(self.X[-1]))
-        Z = numpy.abs(self.X[-1]-numpy.median(self.X))/MADn
-        # print('Z ' + Z.astype(str))
-        if Z > self.threshold:
-            self.isAnomaly = True
+        if(self.MAD<self.minMAD):
+            self.MAD=self.minMAD
+        MADn=1.4826*self.MAD
+        Z=numpy.abs(self.X[-1]-numpy.median(self.X))/MADn
+        self.upperBound=numpy.abs(numpy.median(self.X) + MADn * threshold)
+        self.lowerBound=numpy.abs(numpy.median(self.X) - MADn * threshold)
+        if( Z> threshold):
+            self.isAnomaly=True
+            self.degree=Z // threshold
 
+    def calculateThreshold(self):
+        self.__calcualteMAD()
+        if(self.MAD<self.minMAD):
+            self.MAD=self.minMAD
+        MADn=1.4826*self.MAD
+        self.threshold=numpy.abs(self.X[-1]-numpy.median(self.X))/MADn
+        
 
-def runMAD_onData(data, windowSize):
-    for i in range(0, len(data)-windowSize+1, 1):
-        X = [data[i:i+windowSize] ]
-        # print(X[0])
-        m = mad(X[0], 0.01, 3)
-        m.detectAnomalies()
-        if m.isAnomaly:
-            anomaliesX.append(X_time[i + windowSize])
-            anomaliesY.append(data[i + windowSize])
-            print('anomaly at', i+windowSize)
+def runMAD_onData(data,windowSize, learningWindowSize):
+    threshold=0.0
+    for i in range (0, learningWindowSize-windowSize+1):
+        X = [data[i:i+windowSize]]
+        m=mad(X[0], 0.01)
+        m.calculateThreshold()
+        threshold = max ( m.threshold, threshold )
+        print(threshold)
+    threshold=1.1*threshold
+    for i in range(learningWindowSize-windowSize+1, len(data)-windowSize+1, 1):
+        X = [data[i:i+windowSize]]
+        m=mad(X[0], 0.01)
+        m.detectAnomalies(threshold)
+        if(m.isAnomaly==True):
+            #anomaliesX.append(X_time[i + windowSize])
+            #anomaliesY.append(data[i + windowSize])
+            print('upper bound breach' if X[0][-1]>m.upperBound else 'lower bound breach','anomaly of rank ', m.degree if m.degree<5 else 5, ' at ', i,'upper bound:', m.upperBound,'lower bound:', m.lowerBound ,'value:', X[0][-1])
 
                                           #####
 ###############################################
 
-file_name = 'wczytywanie danych/data/p1.txt'
+file_name = 'wczytywanie danych/data.txt'
 data = pd.read_csv(file_name, ';')  # wczytanie pliku
 X = data.values[:, 1]  # wczytanie pomiarow
 
@@ -68,7 +82,7 @@ X_time = []
 for t in data.values[:, 0]:
     X_time.append(dt.strptime(t, '%Y-%m-%d %H:%M:%S')) # wczytanie formatu czasu
 
-runMAD_onData(X, 700)  # 96 - okienko powinno byc 1 dzien, 2880 - miesiac
+runMAD_onData(X, 700, 4200)  # 96 - okienko powinno byc 1 dzien, 2880 - miesiac
 
 plt.figure(figsize=(10, 5))
 plt.plot_date(anomaliesX, anomaliesY, 'ro', markersize=5)  # wyswietlanie annomalii
